3204b7abf8bf2633a55abf5e23caa5871f0a4797
Akarsh- <akarshalu@gmail.com>
2014-06-21 23:45:25 +0530

    Update HitDirection.cs
    updated to move ball with force given by user....
    force increases by pressing space bar 
    energy bar also in scene

--- FIFADEMo/Assets/Scripts/HitDirection.cs
+++ FIFADEMo/Assets/Scripts/HitDirection.cs
@@ -1,33 +1,51 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class HitDirection : MonoBehaviour {
 
 	Vector3 elevation ; 
-	Vector3 elevationAngle;
-	int throwForce;
+	public Vector3 elevationAngle;
+	public float throwForce;
 	HitCalculation anglebtw;
 	float PointerAForce;
+	public Vector2 size = new Vector2(80,20);
+	public float barDisplay = 0.0f;
 	void Start () {
 	  anglebtw= GameObject.FindWithTag("Pointer").GetComponent <HitCalculation>();// as HitCalculation; 44444
 
 		elevationAngle = new  Vector3(anglebtw.angleBetween,1,2);
 		Debug.Log ("elevation angle"+ elevationAngle);
 		elevation = Quaternion.Euler(elevationAngle) * transform.forward;
-		throwForce = 1000;
+		throwForce = 5.0f;
+
 	}
+	public float spe = 0.25f;
 
-
-	void Update () {
+	void Update () 
+	{
 	//	HitCalculation anglebtw= GetComponent ("HitCalculation") as HitCalculation; 
 
 		Debug.Log ("angle between " + anglebtw.angleBetween);
-		elevationAngle = new Vector3((anglebtw.angleBetween/5),5,10);
-
-		if(Input.GetKeyDown(KeyCode.A)){
-			//	this.rigidbody.AddForce(elevation * throwForce);
-			rigidbody.velocity = transform.TransformDirection(elevationAngle * 0.3f);
+		elevationAngle = new Vector3((anglebtw.angleBetween/15),5,5);
+		if(Input.GetKey(KeyCode.Space))
+		{
+			//throwForce += 0.2f;
+			spe += 0.005f;
+			barDisplay+=0.005f;
 		}
-	
+		if(Input.GetKeyDown(KeyCode.A)){
+			rigidbody.AddForce(elevation * throwForce);
+			rigidbody.velocity = transform.TransformDirection(elevationAngle * spe);
+		}
+	}
+	void OnGUI()
+	{
+		GUI.BeginGroup (new Rect (6*Screen.width/24, 150, 80,20));
+		GUI.Box (new Rect (0,0, 80,20),"FORCE");
+		GUI.BeginGroup (new Rect (0, 0, barDisplay*1000, 20));
+		GUI.Box (new Rect (0,0,80,20),"FORCE");
+		GUI.EndGroup ();
+		
+		GUI.EndGroup ();
 	}
 }
